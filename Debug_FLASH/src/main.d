b8425b6181f56121f3e8d3cc77aa9ff17f56fdce
Julián Aragón <fernando.aragon@nxp.com>
2025-03-18 16:12:03 -0600

    second commit

---
+++ Debug_FLASH/src/main.d
@@ -0,0 +1,37 @@
+src/main.o: ../src/main.c \
+ C:/Users/nxf88257/workspaceS32DS.3.5_RTD_5.0.0/hello_world_s32k144/board/sdk_project_config.h \
+ C:/Users/nxf88257/workspaceS32DS.3.5_RTD_5.0.0/hello_world_s32k144/board/clock_config.h \
+ C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/clock.h \
+ C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/device_registers.h \
+ C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/common/s32_core_cm4.h \
+ C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/S32K144/include/S32K144.h \
+ C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/S32K144/include/S32K144_features.h \
+ C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/devassert.h \
+ C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/status.h \
+ c:\nxp\s32ds.3.5_rtd_5.0.0\s32ds\software\s32sdk_s32k1xx_rtm_4.0.1\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h \
+ C:/Users/nxf88257/workspaceS32DS.3.5_RTD_5.0.0/hello_world_s32k144/board/pin_mux.h \
+ C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/pins_driver.h
+
+C:/Users/nxf88257/workspaceS32DS.3.5_RTD_5.0.0/hello_world_s32k144/board/sdk_project_config.h:
+
+C:/Users/nxf88257/workspaceS32DS.3.5_RTD_5.0.0/hello_world_s32k144/board/clock_config.h:
+
+C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/clock.h:
+
+C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/device_registers.h:
+
+C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/common/s32_core_cm4.h:
+
+C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/S32K144/include/S32K144.h:
+
+C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/S32K144/include/S32K144_features.h:
+
+C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/devassert.h:
+
+C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/devices/status.h:
+
+c:\nxp\s32ds.3.5_rtd_5.0.0\s32ds\software\s32sdk_s32k1xx_rtm_4.0.1\platform\drivers\src\clock\s32k1xx\clock_s32k1xx.h:
+
+C:/Users/nxf88257/workspaceS32DS.3.5_RTD_5.0.0/hello_world_s32k144/board/pin_mux.h:
+
+C:/NXP/S32DS.3.5_RTD_5.0.0/S32DS/software/S32SDK_S32K1XX_RTM_4.0.1/platform/drivers/inc/pins_driver.h:
